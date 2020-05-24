0ec412a910f2f759cb766985639c52570e50ea0b
AgaevDanik <agaevdaniik@gmail.com>
2020-05-24 11:44:42 +0300

    notifications of order, in Telegram

--- shava/celery.py
+++ shava/celery.py
@@ -1,6 +1,8 @@
+import os
+
 from celery import Celery
 
-
+os.environ['DJANGO_SETTINGS_MODULE'] = 'shava.settings'
 app = Celery("shava")
 app.config_from_object("django.conf:settings")
 app.autodiscover_tasks()
--- shava/tasks.py
+++ shava/tasks.py
@@ -1,6 +1,11 @@
 from .celery import app
+from os import environ
+import requests
 
 
 @app.task
-def send_email():
-    pass
+def send_message_order(text):
+    params = {'chat_id': environ.get('CHAT_ID'), 'text': text}
+    response = requests.post(environ.get('TOKEN') + 'sendMessage', data=params)
+    return response
+
