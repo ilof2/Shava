8662d0677a94747978976764b6977d6686fa89dc
AgaevDanik <agaevdaniik@gmail.com>
2020-05-28 19:02:14 +0300

    rusification front and validation

--- api/views.py
+++ api/views.py
@@ -12,6 +12,9 @@ class CreateOrder(APIView):
 
     def post(self, request):
         data = request.data
+        response = validation(data)
+        if response.status_code != 200:
+            return response
         phone = data.get("phone")
         name = data.get("name")
         address = data.get("address")
@@ -22,11 +25,30 @@ class CreateOrder(APIView):
         order = Order(address=address, message=message, phone_number=phone, customer_name=name, price=0)
         for prod in products_data.values():
             order.price = order.price + (prod.get("price", 0) * prod.get("count"))
-        order.save()
+
+        try:
+            order.save()
+        except Exception:
+            Response(status=500)
+
         for prod in product_objs:
             order.product.add(prod)
-        order.save()
+        try:
+            order.save()
+        except Exception:
+            Response(status=500)
         for prod_id, prod in products_data.items():
             order_count = OrderProductCount(product_id=prod_id, order_id=order.id, count=prod.get("count"))
-            order_count.save()
+            try:
+                order_count.save()
+            except Exception:
+                Response(status=500)
         return Response(status=200)
+
+
+def validation(data):
+    if data.get("phone") and data.get("name") and data.get("address") and data.get("message"):
+        return
+    if len(data.get("message")) > 200:
+        return Response(status=400)
+    return Response(status=400)
