9f567bd0f70a59904497c0838bfbaa33ab1f088c
ilof2 <ilof@tuta.io>
2020-05-28 10:28:25 +0300

    some fixes in api and requirements

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from shava.models import Order, Product
+from shava.models import Order, Product, OrderProductCount
 
 
 class CreateOrder(APIView):
@@ -19,9 +19,13 @@ class CreateOrder(APIView):
         products_data = json.loads(request.data.get("products", '{}'))
         product_ids = products_data.keys()
         product_objs = Product.objects.filter(id__in=product_ids)
-        order = Order(address=address, message=message, phone_number=phone, customer_name=name)
+
+        order = Order(address=address, message=message, phone_number=phone, customer_name=name, price=24)
         order.save()
         for prod in product_objs:
             order.product.add(prod)
         order.save()
+        for prod_id, prod in products_data.items():
+            order_count = OrderProductCount(product_id=prod_id, order_id=order.id, count=prod.get("count"))
+            order_count.save()
         return Response(status=200)
